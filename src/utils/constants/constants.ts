65be350a41482474e9df19608cd68a8ed8be7746
himanshu <himanshu.andel@yahoo.in>
2025-09-17 22:08:04 +0530

    Seperate the constant part from component to constant file.

---
+++ src/utils/constants/constants.ts
@@ -0,0 +1,84 @@
+
+
+export const menuItems = [
+    {
+        title: "MENU",
+        items: [
+            {
+                icon: "home.png",
+                lable: "Home",
+                href: "/",
+            },
+            {
+                icon: "teacher.png",
+                lable: "Teachers",
+                href: "/teachers",
+            },
+            {
+                icon: "student.png",
+                lable: "Students",
+                href: "/students",
+            },
+            {
+                icon: "parent.png",
+                lable: "Parents",
+                href: "/parents",
+            },
+            {
+                icon: "class.png",
+                lable: "Classes",
+                href: "/classes",
+            },
+            {
+                icon: "lesson.png",
+                lable: "Lessons",
+                href: "/lessons",
+            },
+            {
+                icon: "exam.png",
+                lable: "Exams",
+                href: "/exams",
+            },
+            {
+                icon: "assignment.png",
+                lable: "Assignments",
+                href: "/assignments",
+            },
+            {
+                icon: "attendance.png",
+                lable: "Attendance",
+                href: "/attendance",
+            },
+            {
+                icon: "calendar.png",
+                lable: "Calendar",
+                href: "/calendar",
+            },
+            {
+                icon: "announcement.png",
+                lable: "Announcements",
+                href: "/announcements",
+            }
+        ]
+    },
+    {
+        title: "OTHER",
+        items: [
+            {
+                icon: "profile.png",
+                lable: "Profile",
+                href: "/profile",
+            },
+            {
+                icon: "settings.png",
+                lable: "Setting",
+                href: "/setting",
+            },
+            {
+                icon: "logout.png",
+                lable: "Logout",
+                href: "/logout",
+            }
+        ]
+    }
+]
