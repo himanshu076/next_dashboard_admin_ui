8d0d6a1c2d80b15a4705ac51e31c334d151c728a
himanshu <himanshu.andel@yahoo.in>
2025-09-18 01:14:12 +0530

    Created UserCard component & added on admin home page.

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,18 @@
+import { IMAGES } from '@/utils/constants/constants'
+import Image from 'next/image'
+import React from 'react'
+
+const UserCard = ({type}: {type: string}) => {
+  return (
+    <div className='rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1 min-w-[130px]'>
+        <div className='flex justify-between items-center'>
+            <span className='text-[10px] bg-white px-2 py-1 rounded-full text-green-600'>2024/25</span>
+            <Image src={IMAGES.icons.more} width={16} height={16} alt='more' />
+        </div>
+        <h1 className='text-2xl font-semibold my-4'>1,234</h1>
+        <h2 className=' text-sm font-medium text-gray-500'>{type}</h2>
+    </div>
+  )
+}
+
+export default UserCard
