f72125914d9c54e18e8fb7e774e6a9e9cb6a8570
himanshu <himanshu.andel@yahoo.in>
2025-09-28 18:40:29 +0530

    WIP - Creating student page.

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,8 +1,18 @@
+import Announcements from '@/components/Announcements';
+import EventCalender from '@/components/EventCalender';
 import React from 'react'
 
 const StudentPage = () => {
   return (
-    <div>StudentPage</div>
+    <div className='p-4 flex gap-4 flex-col xl:flex-row'>
+      {/* LEFT */}
+      <div className='w-full xl:w-2/3'></div>
+      {/* RIGHT */}
+      <div className='w-full xl:w-1/3 flex flex-col gap-8'>
+        <EventCalender />
+        <Announcements />
+      </div>
+    </div>
   )
 }
 
